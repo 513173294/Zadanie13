8fe54990f5fb83c69d1383498b34d26294beec6f
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-06-10 13:37:32 +0200

    poprawiona petla

--- src/Method.java
+++ src/Method.java
@@ -2,30 +2,34 @@ import java.util.Scanner;
 
 public class Method {
 
-    Scanner scanner = new Scanner(System.in);
 
     int liczby() {
-
-        System.out.println("Podaj liczbę");
-        int liczba = scanner.nextInt();
-        scanner.nextLine();
-
-        while (liczba >= 100 && liczba <= 200 && liczba % 3 == 0) {
+        Scanner scanner = new Scanner(System.in);
+        int liczba = 0;
+int x = 0;
+        do {
+            System.out.println("Podaj liczbę");
+            liczba = scanner.nextInt();
+            scanner.nextLine();
+            if (liczba < 100) {
+                System.out.println("liczba jest za mała");
+                break;
+            }
+            if (liczba > 200) {
+                System.out.println("liczba jest za duża");
+            }
+            if (liczba % 3 != 0) {
+                System.out.println("liczba nie jest podzielna przez 3 ");
+            }
+            if (liczba>=100 && liczba<=200 && liczba%3==0)
             System.out.println("twoja liczba jest ok");
-            break;
+
         }
-        if (liczba < 100) {
-            System.out.println("liczba jest za mała");
-            return liczby();
-        } else if (liczba > 200) {
-            System.out.println("liczba jest za duża");
-            return liczby();
-        }
-        if (liczba % 3 != 0) {
-            System.out.println("liczba nie jest podzielna przez 3 ");
-            return liczby();
-        }
-        return 0;
+
+        while (x==0);
+
+
+        return liczba;
     }
 }
 
--- src/Zadanie13.java
+++ src/Zadanie13.java
@@ -1,7 +1,11 @@
 public class Zadanie13 {
     public static void main(String[] args) {
-Method method= new Method();
-method.liczby();
-    }
+        Method method = new Method();
+//        method.liczby();
+
+        Method2 method2 = new Method2();
+        method2.liczby();
+
+        }
 
 }
